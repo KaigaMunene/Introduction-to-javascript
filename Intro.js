e3a8a30fbf56159293dce3f168d8ab80005f772a
KaigaMunene <kaigamunene@gmail.com>
2021-11-01 13:47:44 +0300

    updated

--- Intro.js
+++ Intro.js
@@ -6,3 +6,12 @@ let b = "20"
 console.log(a == b)
 console.log(a === b)
 
+let age = 20
+if (age < 18) {
+    console.log(" young");
+}
+else {
+    console.log("adult");
+}
+
+console.log(age < 18 ? console.log("young"): console.log("adult"))  //use ofternary operator in if statement//
