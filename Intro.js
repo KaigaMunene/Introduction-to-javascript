eb0ae424f5bcba251b6539f2a1311a7f5862162c
KaigaMunene <kaigamunene@gmail.com>
2021-11-01 15:39:16 +0300

    added switch and while loop

--- Intro.js
+++ Intro.js
@@ -15,3 +15,64 @@ else {
 }
 
 console.log(age < 18 ? console.log("young"): console.log("adult"))  //use ofternary operator in if statement//
+
+let gender = "female"
+let y = gender === "male" ? 0 : 1;
+console.log(y)
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+for (let i = 1000; i <= 1010; i+=2) {
+    console.log(i)
+}
+
+let i = 1000
+while (i <= 1010) {
+    console.log(i);
+    i += 2
+}
+
+let aq = 51
+while (aq <= 55) {
+    console.log(aq)
+    aq +=2 
+}
+
+i = 0;
+do{
+    console.log(i);
+    i+= 2
+}
+while (i<0);
+
+while (i<0){
+    console.log(i)
+}
+
+age = 30
+switch(age){
+    case 10:
+        console.log("Too young");
+        break;
+
+    case 15:
+        console.log("Teen");
+        break;
+    
+    case 18:
+        console.log("young adult");
+        break;
+    
+    case 30:
+        console.log("adult");
+        break;
+
+    case 60:
+        console.log("old")
+        break;
+    
+    default:
+        console.log();
+}
